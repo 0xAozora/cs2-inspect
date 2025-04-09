787c01ded3b4178717b87b7814116dab005c4328
0xAozora <atha.ar@arcor.de>
2025-04-09 17:24:55 +0200

    Add explanation of unsafe Pointer use in example

--- example/main.go
+++ example/main.go
@@ -62,6 +62,16 @@ func main() {
 func inspectItem(h *inspect.Handler, logger *zerolog.Logger) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		// If you are wondering why we are using unsafe.Pointer:
+		// We could be parsing the user payload directly into the Info Struct,
+		// however the user could add extra fields to the struct which we would need to verify in the worst case
+		// That leaves us with two options:
+		// 1. Parse the payload into the Lookup struct, and then copy the data into the Info struct.
+		//    This does work with a few items, but produces more garbage especially for alot of items.
+		// 2. Parse the payload into the Lookup struct, and then use unsafe.Pointer to "convert" it to the Info struct
+		//    This is a bit more risky, but reuses the memory and you can typecast the whole slice for a bulk lookup
+		// We could also have embedded the Lookup struct into the Info struct, however it does not work well with the messagepack code generation
+		// to use it in internal messaging systems
 		request := types.Request{L: []*types.Info{{}}}
 		lookup := (*types.Lookup)(unsafe.Pointer(request.L[0]))
 
